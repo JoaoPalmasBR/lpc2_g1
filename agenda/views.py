8c642f42c7cac783fe4912550110a88d8cb3e4d7
Joao Antonio Santos <23306883+JoaoPalmasBR@users.noreply.github.com>
2017-09-18 20:51:14 -0300

    questao 2.b
    b) Uma rota que retorna as agendas públicas de um usuário pelo seu identificador (id).
    Se eu digitar na url /agendas/usuario/1 deve ser apresentado todas as agendas do
    usuário com identificador 1. (1,0)

--- agenda/views.py
+++ agenda/views.py
@@ -7,9 +7,24 @@ def listaAgenda(request):
     lista = Agenda.objects.all()
     retorno=''
     for agenda in lista:
-        retorno += '<p>Agenda: {}</p><br>'.format(agenda.usuario, agenda.tipo)
+        retorno += '<p>Agenda: {}</p>'.format(agenda.usuario, agenda.tipo)
         lista2 = agenda.compromissos.all()
+        retorno += '<p>Comprimissos: <ul>'
         for c in lista2:
-            retorno += '<p>{}</p>'.format(c.nome)
-        retorno += '<hr>'
+            retorno += '<li>{}, {}</li>'.format(c.nome, c.data)
+        retorno += '</ul></p><hr>'
     return HttpResponse(retorno)
+
+def get_agenda_byID(request,id):
+    lista = Agenda.objects.filter(usuario_id=id)
+    retorno=''
+    for agenda in lista:
+        if agenda.tipo=="PUBLICA":
+            retorno += '<p>Agenda Publica: {}</p>'.format(agenda.usuario)
+            lista2 = agenda.compromissos.all()
+            retorno += '<p>Comprimissos: <ul>'
+            for c in lista2:
+                retorno += '<li>{}, {}</li>'.format(c.nome, c.data)
+            retorno += '</ul></p><hr>'
+    return HttpResponse(retorno)
+    
--- calendario/urls.py
+++ calendario/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from agenda.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^agendas/$', listaAgenda)
+    url(r'^agendas/$', listaAgenda),
+    url(r'^agendas/usuario/(?P<id>[0-9]{1})/', get_agenda_byID)
 ]
